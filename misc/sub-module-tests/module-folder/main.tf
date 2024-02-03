554c8644fb6634e13e9397cfa19f8815cb602c06
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2024-02-03 15:23:32 +0200

    Update main.tf

--- misc/sub-module-tests/module-folder/main.tf
+++ misc/sub-module-tests/module-folder/main.tf
@@ -4,8 +4,8 @@ resource "local_file" "test" {
 }
 
 resource "time_sleep" "wait_seconds" {
-  create_duration = "1s"
-  destroy_duration = "1s"
+  create_duration = "2s"
+  destroy_duration = "2s"
 }
 
 resource "null_resource" "this" {
