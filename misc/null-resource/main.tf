095869afa12fb5b812a6655c12d42ffcffda88e9
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2024-07-02 11:50:24 +0300

    Update main.tf

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  cloud {
+    hostname     = "backend.api.env0.com"
+    organization = "52e69ba9-1596-4565-89f5-9df45cc59ccb.0e757317-5a22-4ba5-97a8-1d45a17b56ca"
+    workspaces {
+      name = "my-first-remote-apply"
+    }
+  }
+}
+
 variable "test" {
   description = "test"
   type        = string
