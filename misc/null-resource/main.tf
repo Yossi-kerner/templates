93977d241c8d7f6e9ce3857309d056820258c8d9
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2024-07-02 14:47:52 +0300

    Update main.tf

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -1,14 +1,3 @@
-terraform {
-  cloud {
-    hostname = "backend-pr16240.api.env0.com"
-    organization = "52e69ba9-1596-4565-89f5-9df45cc59ccb.0e757317-5a22-4ba5-97a8-1d45a17b56ca"
-
-    workspaces {
-      name = "null-22282-67946634"
-    }
-  }
-}
-
 variable "test" {
   description = "test"
   type        = string
