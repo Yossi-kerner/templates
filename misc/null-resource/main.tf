f8d23c9a0612957f4cba316311342016f5a71177
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2024-07-02 12:19:58 +0300

    Update main.tf

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -4,7 +4,7 @@ terraform {
     organization = "52e69ba9-1596-4565-89f5-9df45cc59ccb.0e757317-5a22-4ba5-97a8-1d45a17b56ca"
 
     workspaces {
-      name = "env0f78252"
+      name = "null-22282-67946634"
     }
   }
 }
