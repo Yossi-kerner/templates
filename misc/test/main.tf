087478df18001866b0b98d28b245d09217a1de13
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2024-07-08 14:30:02 +0300

    Update main.tf

--- misc/test/main.tf
+++ misc/test/main.tf
@@ -12,31 +12,31 @@ provider "env0" {
   api_endpoint = "https://api-dev.dev.env0.com"
 }
 
-resource "env0_environment" "example_with_hcl_configuration" {
+resource "env0_environment" "new_env" {
   name        = "new env"
   project_id  = "72002433-9307-40b5-bf28-67b37b12e296"
   template_id = "1ce774a2-71ac-4047-8cb2-da09973af08a"
 }
 
-# resource "env0_configuration_variable" "example" {
-#   name        = "ENVIRONMENT_VARIABLE_NAME"
-#   value       = "example value"
+resource "env0_configuration_variable" "example" {
+  name        = "ENVIRONMENT_VARIABLE_NAME"
+  value       = "example value"
+}
+
+# resource "env0_variable_set" "organization_scope_example" {
+#   name        = "variable-set-example1"
+#   description = "description123"# 
+
+#   variable {
+#     name   = "ENVIRONMENT_VARIABLE_NAME"
+#     value  = "example value"
+#     format = "text"
+#   }
 # }
-
-resource "env0_variable_set" "organization_scope_example" {
-  name        = "variable-set-example1"
-  description = "description123"
-
-  variable {
-    name   = "ENVIRONMENT_VARIABLE_NAME"
-    value  = "example value"
-    format = "text"
-  }
-}
-resource "env0_variable_set_assignment" "assignment" {
-  scope    = "organization"
-  scope_id = "4fbca492-2d85-4fb9-bef2-e87582cd690e"
-  set_ids = [
-    env0_variable_set.organization_scope_example.id,
-  ]
-}
+# resource "env0_variable_set_assignment" "assignment" {
+#   scope    = "organization"
+#   scope_id = "4fbca492-2d85-4fb9-bef2-e87582cd690e"
+#   set_ids = [
+#     env0_variable_set.organization_scope_example.id,
+#   ]
+# }
