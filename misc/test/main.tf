b9d8a0630963225e25bc2553297e1a325e5405de
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2024-07-08 14:02:56 +0300

    Update main.tf

--- misc/test/main.tf
+++ misc/test/main.tf
@@ -18,35 +18,34 @@ resource "env0_environment" "example_with_hcl_configuration" {
   template_id = "1ce774a2-71ac-4047-8cb2-da09973af08a"
 }
 
-resource "env0_configuration_variable" "example" {
-  name        = "ENVIRONMENT_VARIABLE_NAME"
-  value       = "example value"
-  description = "Here you can fill description for this variable, note this field have limit of 255 chars"
+# resource "env0_configuration_variable" "example" {
+#   name        = "ENVIRONMENT_VARIABLE_NAME"
+#   value       = "example value"
+#   description = "Here you can fill description for this variable, note this field have limit of 255 chars"
+# }
+
+resource "env0_variable_set" "organization_scope_example" {
+  name        = "variable-set-example1"
+  description = "description123"
+
+  variable {
+    name   = "n1"
+    value  = "v1"
+    format = "text"
+  }
+
+  variable {
+    name         = "n1"
+    value        = "v2"
+    type         = "environment"
+    format       = "text"
+    is_sensitive = true
+  }
 }
-
-# resource "env0_variable_set" "organization_scope_example" {
-#   name        = "variable-set-example1"
-#   description = "description123"
-# 
-#   variable {
-#     name   = "n1"
-#     value  = "v1"
-#     format = "text"
-#   }
-# 
-#   variable {
-#     name         = "n1"
-#     value        = "v2"
-#     type         = "environment"
-#     format       = "text"
-#     is_sensitive = true
-#   }
-# }
-
-# resource "env0_variable_set_assignment" "assignment" {
-#   scope    = "organization"
-#   scope_id = data.example_with_hcl_configuration.id
-#   set_ids = [
-#     env0_variable_set.organization_scope_example.id,
-#   ]
-# }
+resource "env0_variable_set_assignment" "assignment" {
+  scope    = "organization"
+  scope_id = data.example_with_hcl_configuration.id
+  set_ids = [
+    env0_variable_set.organization_scope_example.id,
+  ]
+}
