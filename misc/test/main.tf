e744b657263707f6889db234de8c85468c750788
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2024-07-08 13:46:32 +0300

    Update main.tf

--- misc/test/main.tf
+++ misc/test/main.tf
@@ -1,9 +1,35 @@
-data "env0_project" "project" {
-  name = "project"
+terraform {
+  required_providers {
+    env0 = {
+      source = "env0/env0"
+    }
+  }
 }
 
-data "env0_environment" "environment" {
-  name = "environment"
+provider "env0" {
+  api_key    = "ai9hz4teack1oe4f"
+  api_secret = "eSDP825BvlT8qYic5iNWrqaM2s8iql9t"
+  api_endpoint = "https://api-dev.dev.env0.com"
+}
+
+resource "env0_environment" "example_with_hcl_configuration" {
+  name        = "environment with hcl"
+  project_id  = data.env0_project.default_project.id
+  template_id = data.env0_template.example.id
+
+  configuration {
+    name          = "TEST1234"
+    type          = "terraform"
+    value         = <<EOF
+      {
+        a = "world11111"
+        b = {
+          c = "d"
+        }
+      }
+    EOF
+    schema_format = "HCL"
+  }
 }
 
 resource "env0_variable_set" "organization_scope_example" {
@@ -23,47 +49,4 @@ resource "env0_variable_set" "organization_scope_example" {
     format       = "text"
     is_sensitive = true
   }
-
-  variable {
-    name   = "n3"
-    value  = "v3"
-    type   = "terraform"
-    format = "hcl"
-  }
-
-  variable {
-    name   = "n4"
-    value  = "{}"
-    type   = "terraform"
-    format = "json"
-  }
-
-  variable {
-    name            = "n5"
-    dropdown_values = ["o3", "o2"]
-    type            = "terraform"
-    format          = "dropdown"
-  }
-}
-
-resource "env0_variable_set" "project_scope_example" {
-  name        = "variable-set-example2"
-  description = "description123"
-  scope       = "project"
-  scope_id    = data.env0_project.project.id
-
-  variable {
-    name   = "n1"
-    value  = "v1"
-    format = "text"
-  }
-}
-
-resource "env0_variable_set_assignment" "assignment" {
-  scope    = "environment"
-  scope_id = data.env0_environment.id
-  set_ids = [
-    env0_variable_set.project_scope_example.id,
-    env0_variable_set.organization_scope_example.id,
-  ]
 }
