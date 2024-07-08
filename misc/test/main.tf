0fff901f320f871da4607ef967d2229cc9e2f706
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2024-07-08 13:56:13 +0300

    Update main.tf

--- misc/test/main.tf
+++ misc/test/main.tf
@@ -14,39 +14,39 @@ provider "env0" {
 
 resource "env0_environment" "example_with_hcl_configuration" {
   name        = "environment with hcl"
-  project_id  = data.env0_project.default_project.id
-  template_id = data.env0_template.example.id
-
-  configuration {
-    name          = "TEST1234"
-    type          = "terraform"
-    value         = <<EOF
-      {
-        a = "world11111"
-        b = {
-          c = "d"
-        }
-      }
-    EOF
-    schema_format = "HCL"
-  }
+  project_id  = "72002433-9307-40b5-bf28-67b37b12e296"
+  template_id = "1ce774a2-71ac-4047-8cb2-da09973af08a"
 }
 
-resource "env0_variable_set" "organization_scope_example" {
-  name        = "variable-set-example1"
-  description = "description123"
-
-  variable {
-    name   = "n1"
-    value  = "v1"
-    format = "text"
-  }
-
-  variable {
-    name         = "n1"
-    value        = "v2"
-    type         = "environment"
-    format       = "text"
-    is_sensitive = true
-  }
+resource "env0_configuration_variable" "example" {
+  name        = "ENVIRONMENT_VARIABLE_NAME"
+  value       = "example value"
+  description = "Here you can fill description for this variable, note this field have limit of 255 chars"
 }
+
+# resource "env0_variable_set" "organization_scope_example" {
+#   name        = "variable-set-example1"
+#   description = "description123"
+# 
+#   variable {
+#     name   = "n1"
+#     value  = "v1"
+#     format = "text"
+#   }
+# 
+#   variable {
+#     name         = "n1"
+#     value        = "v2"
+#     type         = "environment"
+#     format       = "text"
+#     is_sensitive = true
+#   }
+# }
+
+# resource "env0_variable_set_assignment" "assignment" {
+#   scope    = "organization"
+#   scope_id = data.example_with_hcl_configuration.id
+#   set_ids = [
+#     env0_variable_set.organization_scope_example.id,
+#   ]
+# }
