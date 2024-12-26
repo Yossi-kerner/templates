169dd3d4945245747c0568142bf652385cc7b98f
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2024-12-26 16:07:10 +0200

    Update main.tf

--- azure/import-storage-account/main.tf
+++ azure/import-storage-account/main.tf
@@ -5,3 +5,16 @@ provider "azurerm" {
 import "azurerm_storage_account" "example_storage_account" {
   id = "/subscriptions/b48787a1-7145-425f-99af-62cde6c50e31/resourcegroups/env0-b09jjokn-group/providers/Microsoft.Storage/storageAccounts/stb09jjokn"
 }
+
+# Free-tier Azure Storage Account
+resource "azurerm_storage_account" "storage" {
+  account_tier              = "Standard"
+  account_replication_type = "LRS"
+  
+  # The name must be globally unique, so we'll append a random string.
+  account_kind = "StorageV2"  # General-purpose v2 storage account is recommended.
+  
+  tags = {
+    environment = "dev"
+  }
+}
