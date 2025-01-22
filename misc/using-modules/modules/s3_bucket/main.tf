8dd169f0e7522937bbb8c943093abfdb1742bf29
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2025-01-22 20:03:32 +0200

    Update main.tf

--- misc/using-modules/modules/s3_bucket/main.tf
+++ misc/using-modules/modules/s3_bucket/main.tf
@@ -3,6 +3,6 @@ resource "random_id" "suffix" {
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket = "${var.bucket_name}-${random_id.suffix.hex}"
+  bucket = "${var.bucket_name}-${random_id.suffix.hex}-yossi"
   acl     = "private"  # Static ACL
 }
