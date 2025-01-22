13bdc70d4d119482d2fad20a51c36504201355f8
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2025-01-22 20:01:13 +0200

    Update main.tf

--- misc/using-modules/main.tf
+++ misc/using-modules/main.tf
@@ -1,3 +1,8 @@
+# Configure AWS provider
+provider "aws" {
+  region = "us-east-1"  # Change this to your desired AWS region
+}
+
 module "s3_bucket" {
   source      = "./modules/s3_bucket"
   bucket_name = "my-unique-bucket-name"
