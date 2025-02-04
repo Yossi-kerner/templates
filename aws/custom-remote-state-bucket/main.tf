6a4e3cfa77af4375a71f9e846aa82efe47f7430d
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2025-02-04 10:38:13 +0200

    Update main.tf

--- aws/custom-remote-state-bucket/main.tf
+++ aws/custom-remote-state-bucket/main.tf
@@ -6,6 +6,7 @@ module "remote_state_bucket" {
   source = "github.com/env0/remote-state-bucket-module/aws"
   
   # Module-specific inputs
+  external_id = "ddd43a98-e5a1-46a2-8748-397118c5dfe4"
   bucket_name = "yossi-custom-bucket-for-remote-state"
   region      = "us-west-2"
   acl         = "private"
