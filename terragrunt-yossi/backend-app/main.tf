cb3e939dde3653b1b2f29d487bcf4a0d4e14acac
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2025-02-26 11:09:54 +0200

    Update main.tf

--- terragrunt-yossi/backend-app/main.tf
+++ terragrunt-yossi/backend-app/main.tf
@@ -1,4 +1,32 @@
-resource "null_resource" "null" {
+provider "azurerm" {
+  version = "~> 1.15"
+}
+
+resource "random_string" "random" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+resource "azurerm_resource_group" "group" {
+  name     = "env0-${random_string.random.result}-group"
+  location = "northeurope"
+}
+
+# Free-tier Azure Storage Account
+resource "azurerm_storage_account" "storage" {
+  name                     = "st${random_string.random.result}"
+  resource_group_name       = azurerm_resource_group.group.name
+  location                 = azurerm_resource_group.group.location
+  account_tier              = "Standard"
+  account_replication_type = "LRS"
+  
+  # The name must be globally unique, so we'll append a random string.
+  account_kind = "StorageV2"  # General-purpose v2 storage account is recommended.
+  
+  tags = {
+    environment = "dev"
+  }
 }
 
 output "backend_output" {
