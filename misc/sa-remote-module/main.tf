483519190f1fe3037417ceecfdaaa60172e63977
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2025-03-02 18:32:38 +0200

    Update main.tf

--- misc/sa-remote-module/main.tf
+++ misc/sa-remote-module/main.tf
@@ -2,23 +2,22 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 3.0"  # Ensure you specify the correct provider version
     }
   }
 }
 
 module "storage_account" {
-  source  = "Azure/azurerm/storage-account"
-  version = "~> 4.0"  # Ensure you're using the appropriate version of the module
+  source = "terraform-azurerm-modules/storage-account/azurerm"  # Correct source path for the module
 
-  resource_group_name = "my_resource_group" # Set your resource group name
-  location           = "East US"            # Set your desired Azure region
+  resource_group_name = "my_resource_group"  # Specify your resource group name
+  location           = "East US"            # Specify the region for your storage account
 
-  # Generate a random name for the storage account
+  # Generate a random name for the storage account using random_id
   storage_account_name = "st${random_id.storage_account_prefix.hex}"
 
   account_tier           = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "LRS"  # Locally redundant storage (can change to GRS, etc.)
 
   tags = {
     environment = "dev"
@@ -26,7 +25,7 @@ module "storage_account" {
 }
 
 resource "random_id" "storage_account_prefix" {
-  byte_length = 8
+  byte_length = 8  # Generates a random prefix for the storage account name
 }
 
 output "storage_account_name" {
