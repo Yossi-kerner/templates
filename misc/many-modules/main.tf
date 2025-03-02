d936b7b6b6a488f58b7cdfff0406d48c923d86ca
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2025-03-02 15:27:30 +0200

    Update main.tf

--- misc/many-modules/main.tf
+++ misc/many-modules/main.tf
@@ -6,9 +6,6 @@ provider "aws" {
 # --------- 1. Using a Remote Git Module ---------
 module "vpc_git" {
   source  = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=v3.14.0"  # Remote Git repository with a tag
-  cidr    = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
 }
 
 # --------- 2. Using a Terraform Registry Module ---------
@@ -17,20 +14,14 @@ module "vpc_git" {
 module "vpc_registry" {
   source  = "terraform-aws-modules/vpc/aws"  # Terraform Registry module
   version = "v3.14.0"  # Specific version from the Registry
-  cidr    = "10.2.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
 }
 
 # --------- 3. Using a Module from an HTTP URL ---------
 # Using the terraform-aws-vpc module from GitHub as a .zip file
 
-# module "vpc_http" {
-#   source  = "https://github.com/terraform-aws-modules/terraform-aws-vpc/archive/refs/tags/v3.14.0.zip"  # Module as a ZIP from GitHub
-#   cidr    = "10.3.0.0/16"
-#   enable_dns_support = true
-#   enable_dns_hostnames = true
-# }
+module "vpc_http" {
+  source  = "https://github.com/terraform-aws-modules/terraform-aws-vpc/archive/refs/tags/v3.14.0.zip"  # Module as a ZIP from GitHub
+}
 
 # Example resources that use the outputs from the VPC modules
 
