e6bd99f0e7bd4c514699ddb7d9ee4bebed37cf9c
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2025-03-02 17:05:16 +0200

    Update main.tf

--- misc/many-modules/main.tf
+++ misc/many-modules/main.tf
@@ -1,56 +1,16 @@
-# Configure the OpenTofu provider (AWS as an example)
-provider "aws" {
-  region = "us-west-2"
+# main.tf
+
+module "remote_module" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "3.0.0"  # Specify the version you want
 }
 
-# --------- 1. Using a Remote Git Module ---------
-module "vpc_git" {
-  source  = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=v3.14.0"  # Remote Git repository with a tag
+module "local_module" {
+  source      = "./local_module"
+  instance_id = module.remote_module.instance_id
+  public_ip   = module.remote_module.public_ip
 }
 
-# --------- 2. Using a Terraform Registry Module ---------
-# This example uses a module from the official Terraform Registry.
-
-module "vpc_registry" {
-  source  = "terraform-aws-modules/vpc/aws"  # Terraform Registry module
-  version = "v3.14.0"  # Specific version from the Registry
-}
-
-# --------- 3. Using a Module from an HTTP URL ---------
-# Using the terraform-aws-vpc module from GitHub as a .zip file
-
-module "vpc_http" {
-  source  = "https://github.com/terraform-aws-modules/terraform-aws-vpc/archive/refs/tags/v3.14.0.zip"  # Module as a ZIP from GitHub
-}
-
-# Example resources that use the outputs from the VPC modules
-
-resource "aws_subnet" "subnet_git" {
-  vpc_id     = module.vpc_git.vpc_id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
-}
-
-resource "aws_subnet" "subnet_registry" {
-  vpc_id     = module.vpc_registry.vpc_id
-  cidr_block = "10.2.1.0/24"
-  availability_zone = "us-west-2b"
-}
-
-resource "aws_subnet" "subnet_http" {
-  vpc_id     = module.vpc_http.vpc_id
-  cidr_block = "10.3.1.0/24"
-  availability_zone = "us-west-2c"
-}
-
-output "vpc_id_git" {
-  value = module.vpc_git.vpc_id
-}
-
-output "vpc_id_registry" {
-  value = module.vpc_registry.vpc_id
-}
-
-output "vpc_id_http" {
-  value = module.vpc_http.vpc_id
+output "instance_details" {
+  value = module.local_module.instance_details
 }
