7fbae8f32ab932bc3c1765e87a82ba5368a8d8f3
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2025-03-02 17:04:19 +0200

    Update main.tf

--- misc/many-modules/remote_module/main.tf
+++ misc/many-modules/remote_module/main.tf
@@ -12,3 +12,11 @@ resource "aws_instance" "example" {
     Name = "ExampleInstance"
   }
 }
+
+output "instance_id" {
+  value = aws_instance.example.id
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+}
