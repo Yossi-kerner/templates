290ecfbbff156e4cf029b3c1b4a063c30f86cac2
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2025-03-02 17:25:04 +0200

    Update main.tf

--- misc/many-modules/local_module/main.tf
+++ misc/many-modules/local_module/main.tf
@@ -1,18 +1,18 @@
 # local_module/main.tf
 
-variable "instance_id" {
-  description = "ID of the EC2 instance"
+variable "vm_id" {
+  description = "ID of the Azure VM"
   type        = string
 }
 
 variable "public_ip" {
-  description = "Public IP of the EC2 instance"
+  description = "Public IP of the Azure VM"
   type        = string
 }
 
 output "instance_details" {
   value = {
-    id        = var.instance_id
-    public_ip = var.public_ip
+    vm_id      = var.vm_id
+    public_ip  = var.public_ip
   }
 }
