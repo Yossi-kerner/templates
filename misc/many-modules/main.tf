8d15cc152c1776d06c6b1aa9b11a3ac807be9ef9
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2025-03-02 17:15:12 +0200

    Update main.tf

--- misc/many-modules/main.tf
+++ misc/many-modules/main.tf
@@ -4,15 +4,15 @@ module "remote_module" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "3.0.0"  # Specify the version of the module
   name    = "example-instance"
-  count   = 1  # Corrected: Use `count` instead of `instance_count`
+  count   = 1  # Create 1 instance
   ami     = "ami-0c55b159cbfafe1f0"  # Replace with a valid AMI ID
   instance_type = "t2.micro"
 }
 
 module "local_module" {
   source      = "./local_module"
-  instance_id = module.remote_module.instance_ids[0]  # Accessing the first instance ID in the list
-  public_ip   = module.remote_module.public_ips[0]    # Accessing the first public IP in the list
+  instance_id = module.remote_module[0].instance_id  # Accessing the first instance ID from the list of objects
+  public_ip   = module.remote_module[0].public_ip    # Accessing the first public IP from the list of objects
 }
 
 output "instance_details" {
