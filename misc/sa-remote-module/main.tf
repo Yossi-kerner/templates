42ccc2a79382f2eb6418d8c98939abb60429bae5
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2025-03-02 18:35:10 +0200

    Update main.tf

--- misc/sa-remote-module/main.tf
+++ misc/sa-remote-module/main.tf
@@ -2,32 +2,27 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~> 3.0"  # Ensure you specify the correct provider version
+      version = "~> 3.0"  # Ensure you're using a valid version of the azurerm provider
     }
   }
 }
 
-module "storage_account" {
-  source = "terraform-azurerm-modules/storage-account/azurerm"  # Correct source path for the module
+resource "random_id" "storage_account_prefix" {
+  byte_length = 8  # Generate a random prefix for the storage account name
+}
 
-  resource_group_name = "my_resource_group"  # Specify your resource group name
-  location           = "East US"            # Specify the region for your storage account
-
-  # Generate a random name for the storage account using random_id
-  storage_account_name = "st${random_id.storage_account_prefix.hex}"
-
-  account_tier           = "Standard"
-  account_replication_type = "LRS"  # Locally redundant storage (can change to GRS, etc.)
+resource "azurerm_storage_account" "example" {
+  name                     = "st${random_id.storage_account_prefix.hex}"  # Storage account name must be between 3 and 24 characters
+  resource_group_name       = "my_resource_group"
+  location                 = "East US"
+  account_tier              = "Standard"
+  account_replication_type = "LRS"  # Locally redundant storage (LRS)
 
   tags = {
     environment = "dev"
   }
 }
 
-resource "random_id" "storage_account_prefix" {
-  byte_length = 8  # Generates a random prefix for the storage account name
-}
-
 output "storage_account_name" {
-  value = module.storage_account.name
+  value = azurerm_storage_account.example.name
 }
