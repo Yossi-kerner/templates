02b0ea6175e8c960d4968fe006a2a0d01a1582a7
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2025-03-05 12:18:46 +0200

    Update main.tf

--- terragrunt-yossi/frontend-app/main.tf
+++ terragrunt-yossi/frontend-app/main.tf
@@ -1,20 +1,2 @@
-# main.tf
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-
-  # Optional: Set the backend configuration if needed (e.g., for remote state)
-}
-
-provider "aws" {
-  region = "us-east-1"  # Set your AWS region here
-}
-
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-  bucket = "my-unique-s3-bucket-name-yossi"  # Change to a globally unique name
-  acl    = "private"
+resource "null_resource" "example" {
 }
