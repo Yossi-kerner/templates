b3bb1ecc69be1572240db7615cc40f90a58c968b
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2025-03-09 10:22:07 +0200

    Update main.tf

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.3"
+    }
+  }
+}
+
 variable "test" {
   description = "test"
   type        = string
