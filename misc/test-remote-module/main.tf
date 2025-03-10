393f3ebe03e21bffb8ec08b4fba4294100d1440d
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2025-03-10 14:55:11 +0200

    Update main.tf

--- misc/test-remote-module/main.tf
+++ misc/test-remote-module/main.tf
@@ -14,6 +14,7 @@ provider "aws" {
 }
 
 module "s3_bucket" {
+  version = "3.15.2"
   source = "terraform-aws-modules/s3-bucket/aws"
   bucket = "my-unique-s3-bucket-name-yossi"  # Change to a globally unique name
 }
