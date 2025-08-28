9da228156c6bcbdf801b4a246f9206dbfd057107
Yossi-kerner <72043585+Yossi-kerner@users.noreply.github.com>
2025-08-28 13:58:02 +0300

    Update main.tf

--- aws/hello-world/main.tf
+++ aws/hello-world/main.tf
@@ -6,3 +6,19 @@ provider "aws" {
 terraform {
   required_version = ">= 0.13"
 }
+
+resource "tls_private_key" "private_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "redash-key-pair" {
+  key_name   = "redash-key-${random_uuid.uuid.result}"
+  public_key = tls_private_key.private_key.public_key_openssh
+}
+
+resource "aws_instance" "test-instance" {
+  ami = "ami-0c654c3ab463d22f6"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.redash-key-pair.key_name
+}
